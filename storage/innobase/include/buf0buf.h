06edbdf64b1f909d87fdb12c37c46dedc7647cfb
Marko Mäkelä <marko.makela@mariadb.com>
2021-10-06 13:52:43 +0300

    fixup: add missing TRANSACTIONAL_INLINE

--- storage/innobase/include/buf0buf.h
+++ storage/innobase/include/buf0buf.h
@@ -1539,6 +1539,7 @@ public:
   @tparam watch      whether to allow watch_is_sentinel()
   @param page_id     page identifier */
   template<bool watch= false>
+  TRANSACTIONAL_TARGET
   bool page_hash_contains(const page_id_t page_id)
   {
     const auto fold= page_id.fold();
@@ -1575,6 +1576,7 @@ public:
   This may only be called after !watch_set() and before invoking watch_unset().
   @param id   page identifier
   @return whether the page was read to the buffer pool */
+  TRANSACTIONAL_TARGET
   bool watch_occurred(const page_id_t id)
   {
     const ulint fold= id.fold();
